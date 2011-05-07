bac429d2f661d5c289dc8f1c95f975f291c5e29d
MFox <michelle.guavajam@gmail.com>
2011-05-07 14:32:48 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Themontanawoodshop::Application.config.session_store :cookie_store, :key => '_themontanawoodshop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Themontanawoodshop::Application.config.session_store :active_record_store
